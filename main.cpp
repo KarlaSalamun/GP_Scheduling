e6808b1a28bc73f0b7ad06e3c700dea09612fd06
Karla <karlasalamun9@gmail.com>
2020-06-07 22:59:13 +0200

    increased training set size

--- main.cpp
+++ main.cpp
@@ -27,6 +27,7 @@
 namespace plt = matplotlibcpp;
 
 void generate_csv(std::vector<double> results, std::vector<double> utils, std::string filename );
+void test_utils_wCPU( std::pair<AbstractNode *, AbstractNode *> heuristic );
 
 int main( void )
 {
@@ -36,7 +37,7 @@ int main( void )
 
     TreeConstructor *tc = new TreeConstructor();
 
-    int population_size = 50;
+    int population_size = 10;
     TreePopulation<TreeSolution<AbstractNode *>> *tp =
             new TreePopulation<TreeSolution<AbstractNode *>>( population_size, tc );
     // TODO stavi ovo u funkciju
@@ -57,86 +58,90 @@ int main( void )
             mutation, selection, nsga, nsga1, tp, 50, population_size, 0 );
     ga->get_solution( population, result );
 
+    test_utils_wCPU( result.data );
 
-    std::vector<Task *> pending;
 
-    UunifastCreator *taskc = new UunifastCreator( 3, "./../../test_inputs/test_1.txt", true, 100, 10, 10, 1 );
-    taskc->set_time_slice( 1 );
-    taskc->load_tasks( pending );
+//    std::vector<Task *> pending;
+//
+//    UunifastCreator *taskc = new UunifastCreator( 3, "./../../test_inputs/test_1.txt", true, 100, 10, 10, 1 );
+//    taskc->set_time_slice( 1 );
+//    taskc->load_tasks( pending );
+//
+//    std::vector<double> durations;
+//    for( auto & element : pending ) {
+//        durations.push_back( element->get_duration() );
+//        element->initialize_task();
+//        assert( element->get_duration() != 0 );
+//    }
+//
+//    Scheduler *sched = new Scheduler();
+//    taskc->compute_hyperperiod( pending );
+//    Simulator<AbstractNode *> *sim = new Simulator<AbstractNode *>( 1, taskc->get_hyperperiod(), taskc, sched, true, true );
 
-    std::vector<double> durations;
-    for( auto & element : pending ) {
-        durations.push_back( element->get_duration() );
-        element->initialize_task();
-        assert( element->get_duration() != 0 );
-    }
 
-    Scheduler *sched = new Scheduler();
-    taskc->compute_hyperperiod( pending );
-    Simulator<AbstractNode *> *sim = new Simulator<AbstractNode *>( 1, taskc->get_hyperperiod(), taskc, sched, true, true );
-    sim->set_heuristic( result.data );
-    sim->set_finish_time( taskc->get_hyperperiod() );
-    sim->set_display();
-    sim->set_filename( "./../../test_outputs/comparison.tex" );
-    sim->set_pending( pending );
-    sim->run();
-
-    taskc->set_filename("./../../test_inputs/test_1.txt");
-    taskc->set_task_number(3);
-    taskc->load_tasks( pending );
-    for( auto & element : pending ) {
-        element->initialize_task();
-    }
-    sim->set_finish_time( taskc->get_hyperperiod() );
-    sim->set_pending( pending );
-    sim->run();
-
-    taskc->set_filename("./../../test_inputs/test_2.txt");
-    taskc->set_task_number(2);
-    taskc->load_tasks( pending );
-    for( auto & element : pending ) {
-        element->initialize_task();
-    }
-    sim->set_finish_time( taskc->get_hyperperiod() );
-    sim->set_pending( pending );
-    sim->run();
-
-    taskc->set_filename("./../../test_inputs/test_3.txt");
-    taskc->set_task_number(3);
-    taskc->load_tasks( pending );
-    for( auto & element : pending ) {
-        element->initialize_task();
-    }
-    sim->set_finish_time( taskc->get_hyperperiod() );
-    sim->set_pending( pending );
-    sim->run();
-
-    std::vector<double> utils;
-    std::vector<double> results;
-
-    for( int overload = 90; overload <= 160; overload = overload + 5 ) {
-        std::string tmp = "./../../test_inputs/" + std::to_string( overload ) + ".txt";
-        taskc->set_filename( tmp );
-        taskc->load_tasks( pending );
-//        overload_values.push_back( overload );
-        double util = 0;
-//        taskc->set_overload( overload );
-//        taskc->compute_overloaded( pending, durations );
-        for( auto & element : pending ) {
-            element->initialize_task();
-            util += element->get_duration() / element->get_period();
-        }
-        printf( "%f\n", util );
-        sim->set_finish_time( taskc->get_hyperperiod() );
-        sim->set_pending( pending );
-        sim->run();
-        sim->compute_mean_skip_factor();
-        printf( "overload: %d\twasted time: %lf\tmean skip factor: %lf\n", overload, sim->get_time_wasted(), sim->get_mean_skip_factor() );
-        utils.push_back( overload / 100. );
-        results.push_back( sim->get_time_wasted() );
-    }
-
-    generate_csv( results, utils, "coevolution_wt.csv" );
+//    sim->set_heuristic( result.data );
+//    sim->set_finish_time( taskc->get_hyperperiod() );
+//    sim->set_display();
+//    sim->set_filename( "./../../test_outputs/comparison.tex" );
+//    sim->set_pending( pending );
+//    sim->run();
+//
+//    taskc->set_filename("./../../test_inputs/test_1.txt");
+//    taskc->set_task_number(3);
+//    taskc->load_tasks( pending );
+//    for( auto & element : pending ) {
+//        element->initialize_task();
+//    }
+//    sim->set_finish_time( taskc->get_hyperperiod() );
+//    sim->set_pending( pending );
+//    sim->run();
+//
+//    taskc->set_filename("./../../test_inputs/test_2.txt");
+//    taskc->set_task_number(2);
+//    taskc->load_tasks( pending );
+//    for( auto & element : pending ) {
+//        element->initialize_task();
+//    }
+//    sim->set_finish_time( taskc->get_hyperperiod() );
+//    sim->set_pending( pending );
+//    sim->run();
+//
+//    taskc->set_filename("./../../test_inputs/test_3.txt");
+//    taskc->set_task_number(3);
+//    taskc->load_tasks( pending );
+//    for( auto & element : pending ) {
+//        element->initialize_task();
+//    }
+//    sim->set_finish_time( taskc->get_hyperperiod() );
+//    sim->set_pending( pending );
+//    sim->run();
+//
+//    std::vector<double> utils;
+//    std::vector<double> results;
+//
+//    for( int overload = 90; overload <= 160; overload = overload + 5 ) {
+//        std::string tmp = "./../../test_inputs/" + std::to_string( overload ) + ".txt";
+//        taskc->set_filename( tmp );
+//        taskc->load_tasks( pending );
+////        overload_values.push_back( overload );
+//        double util = 0;
+////        taskc->set_overload( overload );
+////        taskc->compute_overloaded( pending, durations );
+//        for( auto & element : pending ) {
+//            element->initialize_task();
+//            util += element->get_duration() / element->get_period();
+//        }
+//        printf( "%f\n", util );
+//        sim->set_finish_time( taskc->get_hyperperiod() );
+//        sim->set_pending( pending );
+//        sim->run();
+//        sim->compute_mean_skip_factor();
+//        printf( "overload: %d\twasted time: %lf\tmean skip factor: %lf\n", overload, sim->get_time_wasted(), sim->get_mean_skip_factor() );
+//        utils.push_back( overload / 100. );
+//        results.push_back( sim->get_time_wasted() );
+//    }
+//
+//    generate_csv( results, utils, "coevolution_wt.csv" );
     /*
     GPEvaluateHeuristic *test_function = new GPEvaluateHeuristic( 4 );
     test_function->periodic = true;
@@ -180,4 +185,47 @@ void generate_csv(std::vector<double> results, std::vector<double> utils, std::s
         fprintf( fd, "%lf,%lf\n", utils[i], results[i] );
     }
     fclose( fd );
+}
+
+void test_utils_wCPU( std::pair<AbstractNode *, AbstractNode *> heuristic )
+{
+    UunifastCreator *taskc = new UunifastCreator( 3, "./../../test_inputs/test_1.txt", true, 20, 4, 2, 1 );
+    Scheduler *sched = new Scheduler();
+    Simulator<AbstractNode *> *sim = new Simulator<AbstractNode *>( 1, taskc->get_hyperperiod(), taskc, sched, true, false );
+
+    sim->set_heuristic( heuristic );
+    std::vector<double> utils;
+    std::vector<double> results;
+
+    std::vector<Task *> test_tasks;
+
+    for( size_t i=0; i<=14; i++ ) {
+        utils.push_back( 0.90 + i * 0.05 );
+    }
+
+    std::vector<double> mean_qos;
+    std::vector<double> actual_utils;
+
+    for( size_t i=0; i<utils.size(); i++ ) {
+        taskc->set_overload(utils[i]);
+        taskc->set_task_number(6);
+//        double sum = 0;
+        for (size_t j = 0; j < 100; j++) {
+            do {
+                taskc->create_test_set(test_tasks);
+                taskc->compute_hyperperiod( test_tasks );
+            } while( taskc->get_hyperperiod() > 10000 );
+            double tmp_util = 0;
+            for( auto & element : test_tasks ) {
+                tmp_util += static_cast<double>( element->get_duration() ) / static_cast<double>( element->get_period() ) ;
+            }
+            actual_utils.push_back( tmp_util );
+            sim->set_pending(test_tasks);
+            sim->set_finish_time(taskc->get_hyperperiod());
+            sim->run();
+            mean_qos.push_back( sim->get_time_wasted() );
+        }
+    }
+
+    generate_csv( mean_qos, actual_utils, "coev_wCPU.csv" );
 }
--- src/GPEvaluate_NSGA.cpp
+++ src/GPEvaluate_NSGA.cpp
@@ -8,7 +8,7 @@
 void GPEvaluate_NSGA::get_value_NSGA( TreeSolution<AbstractNode *> &solution, std::pair<double, double> &fitness, bool display )
 {
     Scheduler *sched = new Scheduler();
-    UunifastCreator *tc = new UunifastCreator( task_number, "./../../test_inputs/comparison.txt", false, 10, 1, 1, 1 );
+    UunifastCreator *tc = new UunifastCreator( task_number, "./../../test_inputs/comparison.txt", false, 20, 4, 2, 1 );
     //    tc->create_test_set( test_tasks );
     //    tc->write_tasks( test_tasks );
     tc->load_tasks( test_tasks );
@@ -16,17 +16,53 @@ void GPEvaluate_NSGA::get_value_NSGA( TreeSolution<AbstractNode *> &solution, st
 
     Simulator<AbstractNode *> *simulator = new Simulator<AbstractNode *>( 1, 10, tc, sched, true, false );
     simulator->set_heuristic( solution.data );
-    simulator->load();
-    simulator->set_finish_time( tc->get_hyperperiod() );
-    simulator->run();
+//    simulator->load();
+//    simulator->set_finish_time( tc->get_hyperperiod() );
+//    simulator->run();
 
-    if( display ) {
-        simulator->display_info();
+    std::vector<double> skip;
+    std::vector<double> gini;
+
+    std::vector<double> utils = { 0.90, 1, 1.1, 1.2, 1.3, 1.4 };
+
+    for( size_t i = 0; i<5; i++) {
+        for( size_t j=0; j<utils.size(); j++ ) {
+            tc->set_overload( utils[j] );
+            tc->set_task_number( 6 );
+           do {
+                tc->create_test_set(test_tasks);
+                tc->compute_hyperperiod( test_tasks );
+            } while( tc->get_hyperperiod() > 10000 );
+            simulator->set_pending( test_tasks );
+            simulator->set_finish_time( tc->get_hyperperiod() );
+            simulator->run();
+            if( display ) {
+                simulator->display_info();
+            }
+            gini.push_back( simulator->compute_gini_coeff() );
+            assert( simulator->compute_gini_coeff() == simulator->compute_gini_coeff() );
+            assert( simulator->compute_skip_fitness() == simulator->compute_skip_fitness() );
+            skip.push_back( simulator->compute_skip_fitness() );
+        }
     }
 
-    fitness = std::make_pair( simulator->compute_deviation(), -simulator->compute_skip_fitness() );
+    if( islessequal( fabs( compute_mean_fitness( skip ) ), 0.1 ) ) {
+        fitness = std::make_pair( 10, -10 );
+    }
+
+    fitness = std::make_pair( compute_mean_fitness( gini ), -compute_mean_fitness( skip ) );
 
     delete simulator;
     delete sched;
     delete tc;
+}
+
+double GPEvaluate_NSGA::compute_mean_fitness( std::vector<double> values )
+{
+    double sum = 0;
+    for( size_t i=0; i<values.size(); i++ ) {
+        sum += values[i];
+        assert( values[i]== values[i] );
+    }
+    return sum / values.size();
 }
